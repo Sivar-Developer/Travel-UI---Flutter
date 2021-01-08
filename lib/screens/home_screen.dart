d286339698ea6bfe331271b15ae93a2b60dab869
Sivar Sarkawt <fragmatica.dc@gmail.com>
2021-01-09 01:37:46 +0300

    HomeScreen title

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,6 +8,23 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+          padding: const EdgeInsets.symmetric(vertical: 30.0),
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0, right: 100.0),
+              child: Text('What would you like to find?',
+                style: const TextStyle(
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
