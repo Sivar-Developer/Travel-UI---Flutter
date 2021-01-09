e84f3aef328d1611e7b2c23b95dd28bfb9081ddf
Sivar Sarkawt <fragmatica.dc@gmail.com>
2021-01-09 16:54:46 +0300

    Destination screen

---
+++ lib/screens/destination_screen.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_travel_ui_starter/models/destination_model.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class DestinationScreen extends StatefulWidget {
+  final Destination destination;
+
+  const DestinationScreen({
+    Key key,
+    @required this.destination
+  }) : super(key: key);
+
+  @override
+  _DestinationScreenState createState() => _DestinationScreenState();
+}
+
+class _DestinationScreenState extends State<DestinationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Stack(
+            children: [
+              Container(
+                height: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30.0),
+                  boxShadow: [BoxShadow(color: Colors.black26, offset: Offset(0.0, 2.0), blurRadius: 6.0)] 
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(30.0),
+                  child: Image(
+                    image: AssetImage(widget.destination.imageUrl),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 40.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(
+                      icon: Icon(Icons.arrow_back),
+                      iconSize: 30.0,
+                      color: Colors.black,
+                      onPressed: () => Navigator.pop(context)
+                    ),
+                    Row(
+                      children: [
+                        IconButton(
+                          icon: Icon(Icons.search),
+                          iconSize: 30.0,
+                          color: Colors.black,
+                          onPressed: () => Navigator.pop(context)
+                        ),
+                        IconButton(
+                          icon: Icon(FontAwesomeIcons.sortAmountDown),
+                          iconSize: 30.0,
+                          color: Colors.black,
+                          onPressed: () => Navigator.pop(context)
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
